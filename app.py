4740688f3873e4bd9e0be37b309337fe49e9b433
ecogsdill <emily.cogsdill@gmail.com>
2018-02-24 09:16:57 -0600

    API route is all tidy now

--- app.py
+++ app.py
@@ -23,13 +23,7 @@ app = Flask(__name__)
 # create route that renders index.html template
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-# This route returns all the damn data
-@app.route("/data")
-def data():
-    data=pd.read_csv("DataSources/traffic_data_clean.csv")
-    return(jsonify(data.to_dict()))    
+    return render_template("index.html") 
 
 # This route returns a list of all distinct incident types (for filtering)
 @app.route("/incident_types")
@@ -42,35 +36,39 @@ def incident_types():
         incident_types.append(row.title())
     return(jsonify(incident_types))    
 
-#This route filters data by incident type
-#It casts everything to uppercase so that the input is not case sensitive (e.g., /data/crash urgent)
-#note that it's ok for the route to have spaces in it: 'http://localhost:5000/data/crash urgent' autocorrects the space so that it's http://localhost:5000/data/crash%20urgent, and it works ~~~
-@app.route("/data/<incident_type>")
-def data_by_incident_type(incident_type):
-    data=pd.read_csv("DataSources/traffic_data_clean.csv")
+'''
+Get data with filters 
+DATE FORMAT: needs to be YYYYMMDD e.g. 2018010
+INCIDENT TYPE can be entered in any case, and spaces are ok, you just have to believe
+
+EXAMPLES:
+all data: /api/v1.1
+all data from Jan 1 2018 onwards: /api/v1.1/?start=20180101
+all data before Oct 1 2017: /api/v1.1/?end=20171001
+data between Jan 1 and Jan 2: /api/v1.1/?start=20180101&end=20170102
+all COLLISIONS between Jan 1 and Jan 2: /api/v1.1/?start=20180101&end=20170102&type=collision
+all COLLISIONS across all dates: /api/v1.1/?type=collision
+all CRASH URGENT across all dates: /api/v1.1/?type=crash urgent
+
+hopefully you get the idea
+'''
+
+@app.route("/api/v1.1/")
+def get_data():
+    start = request.args.get('start', default = 00000000, type = int)
+    end = request.args.get('end', default = 99999999, type = int)
+    incident_type = request.args.get('type', default = '*', type = str)
     incident_type_lookup=incident_type.upper()
-    for i in range(0,len(data)):
-        data['Issue Reported'].replace(to_replace=data['Issue Reported'].iloc[i],value=data['Issue Reported'].iloc[i].upper(),inplace=True)
-
-    output=data[data['Issue Reported']==incident_type_lookup]
-    return(jsonify(output.to_dict()))
-
-#pass in start date in the format YYYYMMDD e.g. 20180101
-@app.route("/api/v1.0/<start>")
-def data_by_start_date(start):
-    data=pd.read_csv("DataSources/traffic_data_clean.csv")
-    output=data[data['Published Date as Integer']>int(start)]
-    return(jsonify(output.to_dict()))
-
-@app.route("/api/v1.0/<start>/<end>")
-def data_by_start_and_end_date(start,end):
-    data=pd.read_csv("DataSources/traffic_data_clean.csv")
-    output=data[(data['Published Date as Integer']>=int(start)) & (data['Published Date as Integer']<=int(end))]
-    return(jsonify(output.to_dict()))
-
-#Ideally we would have the route set up so that start date, end date, and incident type are all optional parameters
-#but idk how to do that and I am done working tonight
-#sry y'all (Emily)
+    if incident_type=='*':
+        data=pd.read_csv("DataSources/traffic_data_clean.csv")
+        return(jsonify(data.to_dict()))
+    else:
+        data=pd.read_csv("DataSources/traffic_data_clean.csv")
+        for i in range(0,len(data)):
+            data['Issue Reported'].replace(to_replace=data['Issue Reported'].iloc[i],value=data['Issue Reported'].iloc[i].upper(),inplace=True)
+        data=data[data['Issue Reported']==incident_type_lookup]
+        output=data[(data['Published Date as Integer']>=int(start)) & (data['Published Date as Integer']<=int(end))]
+        return(jsonify(output.to_dict()))
 
 if __name__ == "__main__":
     app.run(debug=True)
